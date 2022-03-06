498f92a2db09bcc1573c62ec3dd040a6153170bc
Hayah Hazem <hayahhazem1999@gmail.com>
2022-03-07 01:51:43 +0200

    eighth commit

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,3 +1,4 @@
-eb use udagram-api --profile default
+eb init --platform node.js-14 --region us-east-1 udagram-api
+eb use udagram-api 
 eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME POSTGRES_PASSWORD=$POSTGRES_PASSWORD POSTGRES_DB=$POSTGRES_DB PORT=$PORT DB_PORT=$DB_PORT POSTGRES_HOST=$POSTGRES_HOST AWS_REGION=$AWS_REGION AWS_PROFILE=$AWS_PROFILE AWS_BUCKET=$AWS_BUCKET URL=$URL JWT_SECRET=$JWT_SECRET
-eb deploy --profile default
+eb deploy 
